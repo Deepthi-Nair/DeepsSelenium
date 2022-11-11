c17a784b6134e05cd4f7de98d93cc0bf5a099d3f
Deepthi-Nair <jdeepti.nair@gmail.com>
2022-11-11 18:31:06 +0530

    added changes for dockerised grid

--- src/main/java/com/qa/AppName/Factory/DriverFactory.java
+++ src/main/java/com/qa/AppName/Factory/DriverFactory.java
@@ -4,14 +4,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -39,7 +46,16 @@ public class DriverFactory {
 		{
 			WebDriverManager.chromedriver().setup();
 			//driver=new ChromeDriver(optionsnmanager.getChromeOptions());
+			if(Boolean.parseBoolean(prop.getProperty("remote")))
+			{
+				//remote browser
+				init_remoteDriver("chrome");
+			}
+			else
+			{
+				//runs local browser
 			tDriver.set(new ChromeDriver(optionsnmanager.getChromeOptions()));
+			}
 		}
 		
 		else if(browser.equalsIgnoreCase("firefox"))
@@ -69,6 +85,49 @@ public class DriverFactory {
 	}
 	
 	
+	private void init_remoteDriver(String browser) {
+		// TODO Auto-generated method stub
+		System.out.println("Running test on remote grid server:  "+browser);
+		if (browser.equalsIgnoreCase("chrome"))
+		{
+			//DesiredCapabilities.this.setCapability(ChromeOptions.CAPABILITY, optionsnmanager.getChromeOptions());
+			DesiredCapabilities cap= new DesiredCapabilities();
+			cap.setBrowserName("chrome");
+			//ChromeOptions chromeOp=new ChromeOptions();
+			//chromeOp.setCapability(ChromeOptions.CAPABILITY, optionsnmanager.getChromeOptions());
+			try {
+				tDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), cap));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+					}
+		else if (browser.equalsIgnoreCase("firefox"))
+		{
+			//DesiredCapabilities.this.setCapability(ChromeOptions.CAPABILITY, optionsnmanager.getChromeOptions());
+			//ChromeOptions chromeOp=new ChromeOptions();
+			DesiredCapabilities cap= new DesiredCapabilities();
+			cap.setBrowserName("firefox");
+			
+			//FirefoxOptions ffOp= new FirefoxOptions();
+			//ffOp.setCapability(ChromeOptions.CAPABILITY, optionsnmanager.getfirefoxOptions());
+			try {
+				tDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), cap));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+					}
+		
+		else {
+			System.out.println("Pass the right browser");
+		}
+		
+	}
+
+
 	public static synchronized WebDriver getDriver()
 	{
 		return tDriver.get();
